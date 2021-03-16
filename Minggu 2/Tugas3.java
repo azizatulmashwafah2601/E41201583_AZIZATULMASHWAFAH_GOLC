c619de3d151bf87f4f1b4aa4969a71004fa1c750
Azizatul Mashwafah <75109998+azizatulmashwafah2601@users.noreply.github.com>
2021-03-16 14:06:04 +0700

    Create Tugas3.java

---
+++ Minggu 2/Tugas3.java
@@ -0,0 +1,24 @@
+package tugas1;
+
+public class Tugas3 {
+    public static void main(String[] args) {
+        boolean gameOver = false;
+        int student = 50, classes = 3;
+        double sales_tax;
+        short number1;
+        
+        int 2beOrNot2be;
+        //kesalahan dalam pendeklarasian variable karena nama variable tidak boleh diawali dengan angka ataupun bilangan
+        float price index;
+        //kesalahan dalam pendeklarasian variable karena nama variable tidak boleh mengandung unsur spasi
+        double lastYear'sPrice;
+        /**kesalahan dalam pendeklarasian variable karena nama variable tidak boleh 
+         * menggunakan simbol kecuali simbol underscore(_), dan dollar ($)
+         */
+        long class;
+        /**
+         *kesalahan dalam pendeklarasian variable karena nama variable tersebut merupakan keyword java 
+         *yang telah telah dipesan untuk didefinisikan oleh Java sebagai tujuan tertentu
+         */
+    }    
+}
