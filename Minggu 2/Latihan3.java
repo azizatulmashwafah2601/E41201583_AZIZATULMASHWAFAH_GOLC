55ceb1080325036228a5635e7f6d78d0cbed6189
Azizatul Mashwafah <75109998+azizatulmashwafah2601@users.noreply.github.com>
2021-03-16 14:03:51 +0700

    Create Latihan3.java

---
+++ Minggu 2/Latihan3.java
@@ -0,0 +1,23 @@
+package latihan1;
+
+        class book {
+            int price;
+            int pages;
+            
+            public void set (int price, int pages) {
+                this.price = price;
+                this.pages = pages;  
+            }
+            public void show () {
+                System.out.println("books information");
+                System.out.println("books price : " + price);
+                System.out.println("number of pages : " + pages);
+            }
+        }
+public class Latihan3 {
+    public static void main(String[] args) {
+                book javabook = new book();
+                javabook.set(60000, 100);
+                javabook.show();
+            }
+        }
