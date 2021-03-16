565532b39c1d43914a947cd089dc971971df1cb8
Azizatul Mashwafah <75109998+azizatulmashwafah2601@users.noreply.github.com>
2021-03-16 14:04:51 +0700

    Create person.java

---
+++ Minggu 2/person.java
@@ -0,0 +1,20 @@
+package person;
+
+public class person {
+    String name;
+    int age;
+    
+    public person(String name, int age) {
+        this.name = name;
+        this.age = age;        
+    }
+    public String getName() {
+        return name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
