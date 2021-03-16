ef9613049e6bce4e4ae09cc223f4204dbd0f1dd2
Azizatul Mashwafah <75109998+azizatulmashwafah2601@users.noreply.github.com>
2021-03-16 14:03:39 +0700

    Create Latihan2.java

---
+++ Minggu 2/Latihan2.java
@@ -0,0 +1,15 @@
+package latihan1;
+
+public class Latihan2 {
+
+    public static void main(String[] args) {
+        
+        String nama = "Indonesia";
+        char abjad = 'a';
+        int a = 6;
+        double b = 6.9;
+        
+        System.out.println(nama);
+        System.out.println(a);
+    }
+}
