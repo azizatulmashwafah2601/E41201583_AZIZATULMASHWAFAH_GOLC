7d69476f76a910be2344ccaf59cb89f09927afd8
Azizatul Mashwafah <75109998+azizatulmashwafah2601@users.noreply.github.com>
2021-03-23 15:27:44 +0700

    Create Latihan1.java

---
+++ Minggu 3/Latihan1.java
@@ -0,0 +1,10 @@
+package latihan1;
+
+public class Latihan1 {
+
+    public static void main(String[] args) {
+        int [] angka = {3, 7, 8, 1, 10, 20, 35};
+        
+        System.out.println("Jumlah elemen array angka = " + angka.length);
+    }    
+}
