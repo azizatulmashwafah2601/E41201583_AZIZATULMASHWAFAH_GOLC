2e07fb852caf9fe2ca0f4fb075fd8ec766d3dfff
Azizatul Mashwafah <75109998+azizatulmashwafah2601@users.noreply.github.com>
2021-03-23 15:29:17 +0700

    Create Tugas1.java

---
+++ Minggu 3/Tugas1.java
@@ -0,0 +1,13 @@
+package tugas1;
+
+public class Tugas1 {
+
+    public static void main(String[] args) {
+        String [] kata = {"Azizatul", "Mashwafah"};
+        
+        for(int i=0; i<kata.length; i++){
+            System.out.print(kata [i]);
+            System.out.print(" ");
+        }            
+    }     
+}
