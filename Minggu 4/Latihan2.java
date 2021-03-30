63cf254c7f97debbd751357fcbb4f85de7bed8cf
Azizatul Mashwafah <75109998+azizatulmashwafah2601@users.noreply.github.com>
2021-03-30 11:07:30 +0700

    Create Latihan2.java

---
+++ Minggu 4/Latihan2.java
@@ -0,0 +1,15 @@
+package latihan1;
+
+public class Latihan2 {
+    public static void main(String[] args) {
+        
+        // membuat variable
+        int i = 0;
+        
+        do {
+            System.out.println("Perulangan ke-" + i);
+            i++;
+        } while (i <= 10);
+        
+    }    
+}
