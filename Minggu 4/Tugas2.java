8ce158d0fc91fbfd93debd18bfb203af5654c356
Azizatul Mashwafah <75109998+azizatulmashwafah2601@users.noreply.github.com>
2021-04-02 09:27:45 +0700

    Update Tugas2.java

--- Minggu 4/Tugas2.java
+++ Minggu 4/Tugas2.java
@@ -2,14 +2,29 @@ package tugas1;
 
 public class Tugas2 {
     public static void main(String[] args) {
-        System.out.println("          Do__While         ");
-        System.out.println("Bilangan kelipatan 2 (1-100)");
-        System.out.println("============================");
-        System.out.print("      ");
+        System.out.println("           Do__While          ");
+        System.out.println(" Bilangan perkalian 2 (0-100) ");
+        System.out.println("==============================");
         
-        for (int i = 1; i<=100; i*=2){
-            System.out.print(i + " ");
-        }
+        int i = 2;
+        int j = 2;
+        
+        do {
+            System.out.print("   " + i);
+            i*=2;
+        } while (i <= 100);
+        
+        System.out.println("");
+        System.out.println("");
+        
+        System.out.println("           Do__While          ");
+        System.out.println(" Bilangan kelipatan 2 (0-100) ");
+        System.out.println("==============================");
+        
+        do {
+            System.out.print(j + " ");
+            j+=2;
+        } while (j <= 100);
         System.out.println("");
     }
 }
