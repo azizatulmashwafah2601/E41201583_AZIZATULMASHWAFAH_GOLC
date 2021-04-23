d6ef955fe6c938c55213a5edb23830361d51a18e
Azizatul Mashwafah <75109998+azizatulmashwafah2601@users.noreply.github.com>
2021-04-23 21:55:41 +0700

    Create B.java

---
+++ Minggu 7/Latihan/abstractt/B.java
@@ -0,0 +1,8 @@
+package abstractt;
+
+public class B extends A{
+    public void method2() {
+        
+        System.out.println("Method abstact yang sudah menjadi konkrit dalam class B");
+    }
+}
