c15f1168d360fbe8dfa44e414170f8a4763d8ba3
Azizatul Mashwafah <75109998+azizatulmashwafah2601@users.noreply.github.com>
2021-04-23 21:56:31 +0700

    Create B.java

---
+++ Minggu 7/Latihan/konstruktor/B.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package konstruktor;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class B extends A {
+    B() {
+        super("test"); // tambahan
+        System.out.println("Konstruktor class B dieksekusi ...");
+    }
+}
