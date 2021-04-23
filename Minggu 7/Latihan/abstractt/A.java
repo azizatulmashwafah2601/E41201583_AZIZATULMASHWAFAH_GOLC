35bfdca96576a580885141b9412c1bc50280f532
Azizatul Mashwafah <75109998+azizatulmashwafah2601@users.noreply.github.com>
2021-04-23 21:55:16 +0700

    Create A.java

---
+++ Minggu 7/Latihan/abstractt/A.java
@@ -0,0 +1,9 @@
+package abstractt;
+
+public abstract class A {
+    abstract public void method2(); // Deklarasi methode abstark.
+    // methode konkrit yang memiliki implementasi
+    public void method1(){
+        System.out.println("Method konkrit dari class A");
+    }
+}
