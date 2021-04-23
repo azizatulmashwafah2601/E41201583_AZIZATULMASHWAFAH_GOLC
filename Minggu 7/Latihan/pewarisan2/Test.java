8aa7523a88b1f5e1c563845b70274e3cdc0189c0
Azizatul Mashwafah <75109998+azizatulmashwafah2601@users.noreply.github.com>
2021-04-23 21:59:43 +0700

    Create Test.java

---
+++ Minggu 7/Latihan/pewarisan2/Test.java
@@ -0,0 +1,9 @@
+package pewarisan2;
+
+public class Test {
+    public static void main(String[] args) {
+        Pelajar mahasiswa = new Pelajar("Azizatul Mashwafah", 172, 75, 
+                "E41201583", "Politeknik Negeri Jember", 98.8);
+        System.out.println(mahasiswa.toString());
+    }    
+}
